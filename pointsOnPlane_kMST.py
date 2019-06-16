1b3cd4373f0d032bdb805cde4e1c9171590abdb9
Marietta Lazana <32538636+mlazana@users.noreply.github.com>
2019-06-16 23:12:56 +0300

    fix circle center

--- pointsOnPlane_kMST.py
+++ pointsOnPlane_kMST.py
@@ -16,29 +16,46 @@ with open(sys.argv[1]) as f:
         #list S containes tuples of x,y coordinates
         S.append((int(c[0]),int(c[1])))
 
+
+    
 # For any pair of points si and sj, 
 # let d(i,j) denote the Euclidean distance between si and sj.
 for pair in itertools.combinations(S,2):
+    x1 = pair[0][0]
+    x2 = pair[1][0]
+    y1 = pair[0][1]
+    y2 = pair[1][1]
     # Creating distances based on euclidean distance for every pair
-    d[pair] = math.sqrt(((pair[0][0]-pair[1][0])**2)+((pair[0][1]-pair[1][1])**2))
+    d[pair] = math.sqrt(((x1 - x2)**2)+((y1-y2)**2))
 
+# print(len(d))
 for pair in d:
-    print(pair)
+
+    x1 = pair[0][0]
+    x2 = pair[1][0]
+    y1 = pair[0][1]
+    y2 = pair[1][1]
+
+    # print("looking for", pair)
     # Constracting a circle
     circleDiameter = math.sqrt(3)*(d[pair])
+    # print("diameter", circleDiameter)
     # Centering the circle at midpont of the line segment
-    cicleCenter = ((pair[0][0]-pair[1][0])/2,(pair[0][1]-pair[1][1])/2)
-    #print("%.2f" % circleDiameter)
-    #print(cicleCenter)
+    circleCenter = ((x2+x1)/2,(y2+y1)/2)
+    # print(circleCenter)
+    # print("___________")
 
     #Let Sc be the subset of S containedin circle
     Sc = 0
-    for p in S:
-        centerDistance = (p[0] - cicleCenter[0])**2 + (p[1] - cicleCenter[1])**2
+    for point in S:
+        # print("this is my pair", pair)
+        centerDistance = (point[0]  - circleCenter[0])**2 + (point[1] - circleCenter[1])**2
+        # print(centerDistance)
         if centerDistance < circleDiameter/2:
             Sc += 1
 
     if Sc < k: 
         continue
     else:
-        print(Sc)
+        # print(Sc)
+        pass
